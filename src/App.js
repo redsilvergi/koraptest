d38556c09846d1018388a2d4274e38ff47770e13
silver <eungi429@gmail.com>
2023-05-23 17:18:13 +0900

    koraptest 1

--- src/App.js
+++ src/App.js
@@ -1,21 +1,31 @@
+import React, { useState } from "react";
 import NATIONAL_PARKS_DATA from "./data.json";
+import JUNCTION_DATA from "./nationalroad_junction";
 import { Map } from "react-map-gl";
 import DeckGL, { GeoJsonLayer } from "deck.gl";
 import "mapbox-gl/dist/mapbox-gl.css";
+import "./App.css";
+
 const MAPBOX_ACCESS_TOKEN =
   "pk.eyJ1IjoicmVkc2lsdmVyNTIyIiwiYSI6ImNsaHl4enpjNzE4N3Eza3Bjemk3MTc1cDYifQ.EL1F3mAAhdlX1du8lCLDGw";
 
 const INITIAL_VIEW_STATE = {
-  latitude: 39.8283,
-  longitude: -98.5795,
-  zoom: 3,
+  latitude: 35.4453,
+  longitude: 127.5795,
+  zoom: 7,
   bearing: 0,
   pitch: 30,
 };
-const MAP_STYLE =
-  "https://basemaps.cartocdn.com/gl/positron-nolabels-gl-style/style.json";
+const MAP_STYLE_DARKMATTER =
+  "https://basemaps.cartocdn.com/gl/voyager-nolabels-gl-style/style.json";
 
 function App() {
+  const [isJunc, setIsJunc] = useState(true);
+
+  const handleJunc = () => {
+    setIsJunc(!isJunc);
+  };
+
   const onClick = (info) => {
     if (info.object) {
       alert(`Name: ${info.object.properties.Name}`);
@@ -39,15 +49,40 @@ function App() {
       autoHighlight: true,
       onClick,
     }),
+    isJunc &&
+      new GeoJsonLayer({
+        id: "national_road_junction",
+        data: JUNCTION_DATA,
+        //Styles
+        stroked: true,
+        filled: true,
+        pointRadiusMinPixels: 2,
+        pointRadiusScale: 1,
+        getPointRadius: (f) => 5,
+        getFillColor: [255, 0, 0],
+        getLineColor: [255, 0, 0, 10000],
+        pickable: true,
+        autoHighlight: true,
+      }),
   ];
   return (
-    <DeckGL
-      initialViewState={INITIAL_VIEW_STATE}
-      controller={true}
-      layers={layers}
-    >
-      <Map mapStyle={MAP_STYLE} mapboxAccessToken={MAPBOX_ACCESS_TOKEN} />
-    </DeckGL>
+    <div className="testc">
+      <div className="container">
+        <button className="toggle-button" onClick={handleJunc}>
+          {isJunc ? "Hide Junction" : "Show Junction"}
+        </button>
+        <DeckGL
+          initialViewState={INITIAL_VIEW_STATE}
+          controller={true}
+          layers={layers}
+        >
+          <Map
+            mapStyle={MAP_STYLE_DARKMATTER}
+            mapboxAccessToken={MAPBOX_ACCESS_TOKEN}
+          />
+        </DeckGL>
+      </div>
+    </div>
   );
 }
 
